00615943da16a19f05d93f7759c4c37b0210d65b
Ling._.T <Lingthang03@gmail.com>
2023-09-19 13:06:47 -0600

    init

---
+++ File2Edges.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.FileReader;
+import java.util.List;
+import java.util.ArrayList;
+
+public class File2Edges {
+
+    private File2Edges() {
+    }
+
+    public static List<Edge> getEdgesFromFile(String filename) {
+        List<String> edgeStrings = getDataFromFile(filename);
+        List<Edge> edges = new ArrayList<Edge>();
+        for (String s : edgeStrings) {
+            edges.add(parseEdgeString(s));
+        }
+        return edges;
+        // read the file and return a list of edges
+    }
+
+    public static List<String> getDataFromFile(String filename) {
+        List<String> result = new ArrayList<String>();
+        try {
+            BufferedReader incoming = new BufferedReader(new FileReader(filename));
+            result.add(incoming.readLine());
+            while (result.get(result.size() - 1) != null) {
+                result.add(incoming.readLine());
+            }
+            incoming.close();
+        } catch (IOException e) {
+            System.err.println("Error: e");
+        }
+        result.remove(result.size() - 1);
+        return result;
+        // read the file and return a list of strings
+        // each string is a line in the file
+    }
+
+    static class Edge {
+        public String startNode;
+        public String endNode;
+        public int weight;
+
+        public Edge(String start, String end, int wt) {
+            this.startNode = start;
+            this.endNode = end;
+            this.weight = wt;
+        }
+
+        public String start() {
+            return this.startNode;
+        }
+
+        public String end() {
+            return this.endNode;
+        }
+
+        public int weight() {
+            return this.weight;
+        }
+    }
+
+    public static Edge parseEdgeString(String edgeString) {
+        String[] parts = edgeString.split(", ", 3);
+        return new Edge(parts[0], parts[1], Integer.parseInt(parts[2]));
+    }
+    // parse the edge string into an Edge object
+    // output will be of the form: Edge("A", "B", 5)
+}
---
+++ Main.java
@@ -0,0 +1,46 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Main {
+    private String dataFileName = "town.txt";
+    private List<File2Edges.Edge> graphEdges;
+
+    public static void main(String[] args) {
+        Main Tree = new Main();
+        Tree.graphEdges = File2Edges.getEdgesFromFile(Tree.dataFileName);
+        Set<String> nodes = new TreeSet<String>();
+
+        // embedded for loop to create the set of nodes
+        for (File2Edges.Edge edge : Tree.graphEdges) {
+            nodes.add(edge.start());
+            nodes.add(edge.end());
+        }
+
+        System.out.println("Graph Nodes:");
+        nodes.forEach(System.out::println);
+
+        System.out.println("Graph Edges:");
+        Tree.graphEdges.forEach(e -> System.out.println(e.start() + " -> " + e.end() + " : " + e.weight()));
+
+    }
+
+    // constructor
+    public Main() {
+        // initialize the list of edges
+        this.graphEdges = new LinkedList<File2Edges.Edge>();
+        try {
+            // create the data file if it doesn't exist
+            File datafile = new File(dataFileName);
+            datafile.createNewFile();
+        } catch (IOException e) {
+            // Auto-generated catch block
+            System.out.println("Error creating test data file." + dataFileName);
+            e.printStackTrace();
+        }
+    }
+
+}
